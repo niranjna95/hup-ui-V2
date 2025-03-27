015a9158749be0dc60e370e9447a4aa74c383ee2
Niranjan Kumar <niranjangiri51295@gmail.com>
2025-03-27 23:06:36 +0530

    logout redirect issued

--- src/components/common/Logout/index.tsx
+++ src/components/common/Logout/index.tsx
@@ -10,8 +10,7 @@ const Logout: NextPage<LogoutProps> = (props) => {
     localStorage.removeItem("at");
     localStorage.removeItem("utz");
     await signOut({
-      callbackUrl:
-        "https://nice-beach-033882b0f.6.azurestaticapps.net/acccount/login",
+      callbackUrl: "/",
     });
   }
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,17 +1,11 @@
-// import { container } from "@/config/ioc";
-// import { TYPES } from "@/config/types";
-// import IUnitOfService from "@/services/interfaces/IUnitOfService";
-
 import { useRouter } from "next/router";
 import { useEffect } from "react";
 
 export default function Home() {
-  //const unitOfService = container.get<IUnitOfService>(TYPES.IUnitOfService);
   const router = useRouter();
   useEffect(() => {
-    router.push(
-      "https://nice-beach-033882b0f.6.azurestaticapps.net/acccount/login"
-    );
+    let redirectUrl = "account/login";
+    router.push(`${redirectUrl}`);
   }, []);
   return (
     <>
